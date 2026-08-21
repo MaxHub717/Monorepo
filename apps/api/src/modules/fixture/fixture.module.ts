917173774895165e99d4cb188584ccaf4a53c014
Martin <officialnexgen2025@gmail.com>
2026-08-21 21:37:40 +0100

    fix(api): register authz module in fixture module

--- apps/api/src/modules/fixture/fixture.module.ts
+++ apps/api/src/modules/fixture/fixture.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module.js';
 import { EventModule } from '../events/event.module.js';
+import { AuthzModule } from '../../common/authz/authz.module.js';
 import { FixtureController } from './fixture.controller.js';
 import { FixtureService } from './fixture.service.js';
 
 @Module({
-  imports: [PrismaModule, EventModule],
+  imports: [PrismaModule, EventModule, AuthzModule],
   controllers: [FixtureController],
   providers: [FixtureService],
   exports: [FixtureService],
